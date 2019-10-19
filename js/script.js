4c1561ab9c021e4cdd0b1457190d0ad1609be7c6
Miguel Herrera <miguelherrera2094@gmail.com>
2019-10-18 19:46:06 -0500

    Actualizacion de clases en javascript

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
+var rutaImagen = [];
+rutaImagen["duendeMistico"] = "./image/cards/SDY-024.png"
+
 class monster_card {
-    constructor(Nombre, Tipo, Nivel, Atk, Def){
+    constructor(Nombre, Familia, Tipo, Nivel, Atk, Def){
         this.nombre = Nombre;
         this.tipo = Tipo;
+        this.familia = Familia;
         this.nivel = Nivel;
         this.ataque = Atk;
         this.defensa = Def;
+
+        this.imagen = new Image();
+        this.imagen.src = rutaImagen[this.nombre];
+    }  
+}
+
+class magic_card {
+    constructor(nombre, tipo, efecto){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.efecto = efecto;
+
+        this.imagen = new Image();
+        this.imagen.src = rutaImagen[this.nombre];
+    }
+}
+
+class trap_card {
+    constructor(nombre, tipo, efecto){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.efecto = efecto;
 
         this.imagen = new Image();
         this.imagen.src = rutaImagen[this.nombre];
